08e885de4f7293164a2c5acb2d83e952ea24eb2a
João Costa <joaovitorcostaoliveira@gmail.com>
2024-01-05 20:21:44 +0000

    Update editar_funcionario.php

--- controlefuncionarios.backend.biz/editar_funcionario.php
+++ controlefuncionarios.backend.biz/editar_funcionario.php
@@ -14,6 +14,7 @@ while ($registro = mysqli_fetch_assoc($resultado))
     <title>Controle Funcionarios</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="./bunitin.css" media="screen" />
 </head>
 <body>
 	<header>
