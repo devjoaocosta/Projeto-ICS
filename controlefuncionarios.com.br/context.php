9c026e2fe1d363547ff0a0c0201f0c28284eba01
João Costa <joaovitorcostaoliveira@gmail.com>
2024-01-05 20:16:25 +0000

    Update context.php

--- controlefuncionarios.com.br/context.php
+++ controlefuncionarios.com.br/context.php
@@ -1,4 +1,4 @@
 <?php
-	$bd = mysqli_connect('192.168.100.20', 'usuario', 's') or die('Incapaz de Conectar');
+	$bd = mysqli_connect('192.168.100.20', 'ics', 'ics') or die('Incapaz de Conectar');
 	mysqli_select_db($bd, 'sitenoticias') or die(mysqli_error($bd));
 ?>
