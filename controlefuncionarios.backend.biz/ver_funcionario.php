bc7ddb9495cf59cd4c9110bc036498a802f3676a
João Costa <joaovitorcostaoliveira@gmail.com>
2024-01-05 20:23:29 +0000

    Update ver_funcionario.php

--- controlefuncionarios.backend.biz/ver_funcionario.php
+++ controlefuncionarios.backend.biz/ver_funcionario.php
@@ -7,6 +7,7 @@ include('context.php');
 	<title>Controle Funcionarios</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="./bunitin.css" media="screen" />
 </head>
 <body>
 	<header>
